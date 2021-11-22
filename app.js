f6c12e788645ba346603d875e7daba11603ca8e3
ElideZavala <zavalavinagreelide@gmail.com>
2021-11-22 11:04:56 -0700

    style-line

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const hamburgerContainer = document.querySelector('#header__main-nav');
-const hamburger = document.querySelector('.header__main-nav--hamburger');
+const hamburger = document.querySelector('.header__main--hamburger');
 const links = document.querySelectorAll('.header__main-nav--links li');
 
 
 hamburger.addEventListener("click", () => {
-	hamburgerContainer.classList.toggle('clicked')
-})
+	hamburgerContainer.classList.toggle("clicked")
+	console.log('hola')
+});
+
