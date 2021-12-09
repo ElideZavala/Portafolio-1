088486dca83b76993706191409d061b6b19e87be
ElideZavala <zavalavinagreelide@gmail.com>
2021-12-08 18:15:24 -0700

    value-input

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const hamburgerContainer = document.querySelector('#header__main-nav');
 const hamburger = document.querySelector('.header__main--hamburger');
 const links = document.querySelectorAll('.header__main-nav--links li');
+const form = document.querySelector('form'); 
+
+const nameInput = document.getElementById('name').value;
+const emailInput = document.getElementById('email').value;
+const subjectInput = document.getElementById('subject').value;
+const messageInput = document.getElementById('message').value;
 
 
 hamburger.addEventListener("click", () => {
@@ -8,4 +14,10 @@ hamburger.addEventListener("click", () => {
 	links.forEach(link => {
 		link.classList.toggle("fade");
 	})
+});
+
+
+form.addEventListener('submit', e => {
+	e.preventDefault(); 
+	console.log(`Mi nombre es ${nameInput} - correo ${emailInput} - asunto ${subjectInput} - Mi mensaje ${messageInput}`);
 });
