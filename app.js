6f0c0f89fa99ef2f41e78009afac3e1519a37f9a
ElideZavala <zavalavinagreelide@gmail.com>
2021-12-09 18:03:51 -0700

    trucaso in HTML

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const hamburgerContainer = document.querySelector('#header__main-nav');
 const hamburger = document.querySelector('.header__main--hamburger');
 const links = document.querySelectorAll('.header__main-nav--links li');
-const form = document.querySelector('form'); 
 
-const nameInput = document.getElementById('name').value;
-const emailInput = document.getElementById('email').value;
-const subjectInput = document.getElementById('subject').value;
-const messageInput = document.getElementById('message').value;
+const $form = document.getElementById('form'); 
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const subjectInput = document.getElementById('subject');
+const messageInput = document.getElementById('message');
 
 
 hamburger.addEventListener("click", () => {
@@ -17,7 +17,52 @@ hamburger.addEventListener("click", () => {
 });
 
 
-form.addEventListener('submit', e => {
+$form.addEventListener('submit', handleSubmit )
+
+function handleSubmit(e) {
 	e.preventDefault(); 
-	console.log(`Mi nombre es ${nameInput} - correo ${emailInput} - asunto ${subjectInput} - Mi mensaje ${messageInput}`);
-});
+	ckeckValue(emailInput.value);
+
+	// const form = new FormData(this);
+	// console.log(form.get('name'))
+
+	const date = {
+		name : nameInput.value, 
+		email : emailInput.value,
+		subject : subjectInput.value,
+		message : messageInput.value 
+	}
+
+	console.log(date.name, date.email);
+}
+
+function ckeckValue(input) {
+	if(input === '') {
+		sendMsg("The mail is required");
+	} else {
+		sent(input.trim());
+		// cleanValue();
+	}
+}
+
+function sent(sentEmail) {
+	sendMsg("The message was send")
+}
+
+function sendMsg(msj) {
+	const small = document.querySelector('.small');
+	small.style.backgroundColor = "#017f96";
+	small.innerText = msj;
+	setTimeout(() => {
+		small.innerText = '';
+	}, 3000);
+}
+
+function cleanValue() {
+	nameInput.value = '';
+	emailInput.value = '';
+	subjectInput.value = '';
+	messageInput.value = '';
+}
+
+
